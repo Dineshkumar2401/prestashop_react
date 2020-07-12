8acc12dacaadaeb1eafd32e814bd076fe605802d
dineshkumar2401 <jkk.dinesh@gmil.com>
2020-07-12 19:10:15 +0530

    first

---
+++ src/head.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+class Head extends Component
+{
+ render(){
+
+ return(
+
+<div>
+        <ul className="menu">
+                <li>HOME</li>
+                <li>PRODUCT</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+                <li>HELP</li>
+        </ul>
+</div>
+ )
+
+ }
+
+
+}
+
+export default Head;
