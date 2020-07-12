716a171b87cb6c40955983a49ad126b2f10e0487
dineshkumar2401 <jkk.dinesh@gmil.com>
2020-07-12 20:22:34 +0530

    App.css

--- src/slider.js
+++ src/slider.js
@@ -6,10 +6,11 @@ class Slider extends Component{
 render(){
 
     return(
-        
+        <>
         <div className="slider-list">
             <div className="slider_1"><img src={logo} width="400px"/></div>
         </div>
+        </>
     )
 
 }
